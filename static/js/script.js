6570b1eb695da6db7c1d40c21478d191574078c6
Stefan Buynov <stefan.buynov@gmail.com>
2016-03-13 22:05:24 +0200

    Fancybox support and 'photo' shortcode

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+(function($){
+  var toTop = $('#toTop').length ? $('#toTop').offset().top - $(window).height() + 20 : 0;
+
+  // Caption
+  $('article').each(function(i){
+    // $(this).find('img').each(function(){
+    //   if ($(this).parent().hasClass('fancybox')) return;
+    //
+    //   var alt = this.alt;
+    //
+    //   if (alt) $(this).after('<span class="caption">' + alt + '</span>');
+    //
+    //   $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
+    // });
+
+    $(this).find('.fancybox').each(function(){
+      $(this).attr('rel', 'article' + i);
+    });
+  });
+
+  if ($.fancybox){
+    $('.fancybox').fancybox();
+  }
+
+})(jQuery);
